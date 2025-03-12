a0aea7f21778ca0e46f075151ab840fd833c6ed1
David <dm987257@gmail.com>
2025-03-12 15:58:24 -0600

    commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='h-screen bg-blue-600'>
+        <p className='text-center text-3xl text.bold'>
+          cat
+        </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/app/iphone/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Agua = () => {
+  return (
+    <div className='h-screen bg-blue-600 md:bg-red-500 lg:bg-green-400'>
+        <p className='text-center text-3xl text.bold'>
+            awita
+        </p>
+    </div>
+  )
+}
+
+export default Agua
